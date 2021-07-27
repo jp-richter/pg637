b55255ad80cdd8324dd774b5dbc0bee11af926ae
la-gaffe <jonathan.schill@tu-dortmund.de>
2021-07-27 14:12:48 +0200

    refactor: add comments

--- src/randomSearch/hp-generator.py
+++ src/randomSearch/hp-generator.py
@@ -2,25 +2,30 @@
 
 import random
 
+
+# RANGES
 batchSizeRange = (4, 4096)
 sampleUsageRange = (3, 30)
 
+# this is on a log-scale
 lrActorRange = (-3, -6)
 lrCriticRange= (-3, -6)
 
 startStdRange = (-0.4, -1.0)
 epsilonRange = (0.1, 0.3)
 
+# SAMPLE
 batchSize = random.sample(range(batchSizeRange[0], batchSizeRange[1]), 1)[0]
 sampleUsage = random.sample(range(sampleUsageRange[0], sampleUsageRange[1]), 1)[0]
 
 lrActor = str(round(10**random.uniform(lrActorRange[0], lrActorRange[1]), 7)) + "f"
 lrCritic = str(round(10**random.uniform(lrCriticRange[0], lrCriticRange[1]), 7)) + "f"
 
-
 startStd = str(round(random.uniform(startStdRange[0], startStdRange[1]), 7)) + "f"
 epsilon = str(round(random.uniform(epsilonRange[0], epsilonRange[1]), 7)) + "f"
 
+
+# GENERATE FILE
 outputStr = ""
 
 with open("HParamsSingletonTemplate.h") as f:
